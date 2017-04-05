80fdde876188d250355f0a357f45bd86943f661c
ccpw_cn <76935144@qq.com>
2017-04-05 19:34:58 +0800

    加上详细的说明

--- src/main/java/com/sinoiov/lhjh/HelloProgram.java
+++ src/main/java/com/sinoiov/lhjh/HelloProgram.java
@@ -11,7 +11,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class HelloProgram {
     public static void main(String[] args) {
+        // beans.xml, 这是一个配置文件，在这里声明Java bean
+        // 在这里，我们通过读取beans.xml文件来创建一个应用程序上下文对象
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // IoC容器中，其作用是作为第三种角色，它的任务是创建 beans.xml文件中声明的 Java Bean 对象。
         HelloWorldService service = context.getBean("helloWorldService", HelloWorldService.class);
         HelloWorld helloWorld = service.getHelloWorld();
         helloWorld.sayHello();

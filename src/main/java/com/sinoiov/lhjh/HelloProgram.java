b4069430075ba34f8d74f3417572c856e420d316
ccpw_cn <76935144@qq.com>
2017-04-06 10:04:52 +0800

    增加易于理解的注释文档

--- src/main/java/com/sinoiov/lhjh/HelloProgram.java
+++ src/main/java/com/sinoiov/lhjh/HelloProgram.java
@@ -34,6 +34,8 @@ public class HelloProgram {
     }
 
     private static void method2() {
+        // HelloProgram这个类、main这个方法、乃至method2这个方法，都根本不知道执行探险任务的到底是哪个类
+        // 他们仅仅是从knights.xml中加载应用程序的上下文，然后调用embarkOnQuest方法即可，他们甚至不知道BraveKnight的存在
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("knights.xml");
         Knight knight = (Knight) context.getBean("knight");
         knight.embarkOnQuest();

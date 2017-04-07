2ec8d8c55fcebfc92775ec9a05a7be3f57c9b740
ccpw_cn <76935144@qq.com>
2017-04-07 14:02:11 +0800

    消除样板式代码示例增加注释

--- src/main/java/com/sinoiov/lhjh/impl/EmployProcessor.java
+++ src/main/java/com/sinoiov/lhjh/impl/EmployProcessor.java
@@ -14,11 +14,12 @@ import java.sql.*;
  * Created by lidawei on 2017/4/7.
  */
 public class EmployProcessor {
-    DataSource dataSource = MyDataSourceFactory.getMySQLDataSource();
+    private DataSource dataSource = MyDataSourceFactory.getMySQLDataSource();
 
     /**
      * 这是一个传统的处理MySQL连接查询数据的做法，里面和业务相关的代码很少，它有大量的代码是冗余的，尤其是在许多地方要多次
      * 连接数据库的时候，我们会发现相似的代码何其之多。。。
+     *
      * @param id 指定雇员id
      * @return 获得雇员信息
      */
@@ -42,6 +43,28 @@ public class EmployProcessor {
             return employee;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         return null;
@@ -50,21 +73,25 @@ public class EmployProcessor {
     /**
      * 在这里我们使用Spring提供的JdbcTemplate（它利用了JDK5的JdbcTemplate实现），使得执行数据库操作时避免传统的JDBC样板
      * 代码成为了可能。写这个方法的程序员，只需要关注查询雇员信息的业务逻辑就好了，不需要去迎合JDBC驱动的API上的各种约束。
+     *
      * @param id 指定雇员id
      * @return 获得雇员信息
      */
     public Employee getEmployeeById2(long id) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        return jdbcTemplate.queryForObject("SELECT id, firstName, lastName, salary FROM employee WHERE id=?", new RowMapper<Employee>() {
-            @Override
-            public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Employee employee = new Employee();
-                employee.setId(rs.getLong("id"));
-                employee.setFirstName(rs.getString("firstName"));
-                employee.setLastName(rs.getString("lastName"));
-                employee.setSalary(rs.getBigDecimal("salary"));
-                return employee;
-            }
-        }, id);
+        return jdbcTemplate.queryForObject(
+                "SELECT id, firstName, lastName, salary FROM employee WHERE id=?", // 第一个参数：SQL查询语句
+                new RowMapper<Employee>() {  // 第二个参数：匿名内部类，需要对mapRow重新进行实现
+                    @Override
+                    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Employee employee = new Employee();
+                        employee.setId(rs.getLong("id"));
+                        employee.setFirstName(rs.getString("firstName"));
+                        employee.setLastName(rs.getString("lastName"));
+                        employee.setSalary(rs.getBigDecimal("salary"));
+                        return employee;
+                    }
+                },
+                id); // 第三个参数：查询ID
     }
 }

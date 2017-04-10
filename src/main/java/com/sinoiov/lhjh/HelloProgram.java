99927472441000357446693c7f295fabaccb32e7
ccpw_cn <76935144@qq.com>
2017-04-10 10:36:22 +0800

    仅保留能说明问题的代码，其他的冗余代码都删除

--- src/main/java/com/sinoiov/lhjh/HelloProgram.java
+++ src/main/java/com/sinoiov/lhjh/HelloProgram.java
@@ -1,7 +1,5 @@
 package com.sinoiov.lhjh;
 
-import com.sinoiov.lhjh.service.HelloWorld;
-import com.sinoiov.lhjh.service.HelloWorldService;
 import com.sinoiov.lhjh.service.Knight;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,26 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class HelloProgram {
     public static void main(String[] args) {
-        method1();
         method2();
-    }
-
-    private static void method1() {
-        // beans.xml, 这是一个配置文件，在这里声明Java bean
-        // 在这里，我们通过读取beans.xml文件来创建一个应用程序上下文对象
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-        // IoC容器中，其作用是作为第三种角色，它的任务是创建 beans.xml文件中声明的 Java Bean 对象。
-        // helloWorldService这个字符串作为bean id，已经在beans.xml中声明了，它对应了一个Java Bean，所以能够实例化
-        HelloWorldService service = context.getBean("helloWorldService", HelloWorldService.class);
-
-        // 得到被注入的成员变量的引用
-        HelloWorld helloWorld = service.getHelloWorld();
-
-        // 调用创建出来的对象的方法
-        helloWorld.sayHello();
-
-        context.close();
     }
 
     private static void method2() {

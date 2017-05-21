146c2922ac60665cd2b81a2c35972a4d5d984880
ccpw_cn <76935144@qq.com>
2017-05-21 12:21:30 +0800

    运行成功

--- src/main/java/com/sinoiov/lhjh/controller/GoodsController.java
+++ src/main/java/com/sinoiov/lhjh/controller/GoodsController.java
@@ -4,6 +4,7 @@ import com.sinoiov.lhjh.beans.GenericResponse;
 import com.sinoiov.lhjh.entity.GoodsEntity;
 import com.sinoiov.lhjh.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

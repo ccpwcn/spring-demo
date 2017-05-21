d6677ffe37a5ac58288311b8f9d10597171fd0a8
ccpw_cn <76935144@qq.com>
2017-05-21 11:11:55 +0800

    Controller写好了。

--- src/main/java/com/sinoiov/lhjh/controller/GoodsController.java
+++ src/main/java/com/sinoiov/lhjh/controller/GoodsController.java
@@ -1,7 +1,13 @@
 package com.sinoiov.lhjh.controller;
 
+import com.sinoiov.lhjh.beans.GenericResponse;
+import com.sinoiov.lhjh.entity.GoodsEntity;
+import com.sinoiov.lhjh.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 商品类前端接口控制器
@@ -10,4 +16,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/goods")
 public class GoodsController {
+    @Autowired
+    private GoodsService goodsService;
+
+    @RequestMapping(value = "/add")
+    public @ResponseBody
+    GenericResponse add(@RequestBody GoodsEntity entity) {
+        GenericResponse response = new GenericResponse();
+        goodsService.add(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/remove")
+    public @ResponseBody
+    GenericResponse remove(@RequestBody GoodsEntity entity) {
+        GenericResponse response = new GenericResponse();
+        goodsService.remove(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/update")
+    public @ResponseBody
+    GenericResponse update(@RequestBody GoodsEntity entity) {
+        GenericResponse response = new GenericResponse();
+        goodsService.update(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/queryByName")
+    public @ResponseBody
+    GenericResponse queryByName(String goodsName) {
+        GenericResponse response = new GenericResponse();
+        goodsService.queryByName(goodsName);
+        return response;
+    }
 }
--- src/main/java/com/sinoiov/lhjh/controller/UserController.java
+++ src/main/java/com/sinoiov/lhjh/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.sinoiov.lhjh.controller;
 
+import com.sinoiov.lhjh.beans.GenericResponse;
+import com.sinoiov.lhjh.entity.UserEntity;
+import com.sinoiov.lhjh.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 用户相关接口
@@ -10,4 +15,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/add")
+    public @ResponseBody
+    GenericResponse add(UserEntity entity) {
+        GenericResponse response = new GenericResponse();
+        userService.add(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/remove")
+    public @ResponseBody
+    GenericResponse remove(UserEntity entity) {
+        GenericResponse response = new GenericResponse();
+        userService.remove(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/update")
+    public @ResponseBody
+    GenericResponse update(UserEntity entity) {
+        GenericResponse response = new GenericResponse();
+        userService.update(entity);
+        return response;
+    }
+
+    @RequestMapping(value = "/queryUserInfoById")
+    public @ResponseBody
+    GenericResponse<UserEntity> queryUserInfoById(String userId) {
+        GenericResponse response = new GenericResponse();
+        userService.queryUserInfoById(userId);
+        return response;
+    }
 }
